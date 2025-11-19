997bbf24db314e5f547ef5110264e0f01f3abdfa
NTRAGET <86812363+NTRAGET@users.noreply.github.com>
2025-11-19 19:45:31 +0800

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,44 @@
+#Tegar Computer - Bojongsoang Electronic Center
+
+from abc import ABC, abstractmethod   #abstrak
+
+class Perangkat(ABC): #inherit kelas ABC nya dari import ABC, Abstrac Base Class
+    @abstractmethod
+    def info(self):
+        pass
+
+class Laptop(Perangkat):
+    def __init__(self, merk, ram, harga):
+        self.merk = merk  #public
+        self._ram = ram   #protected  (cuman satu)
+        self.__harga = harga #private (doublespasi)
+
+    def getHarga(self):
+        return self.__harga #ini private, ambil harga di private
+
+    def setHarga(self, harga_baru):
+        if harga_baru > 0:   #biar gk negatif, kalau dibawah 0 ada -1, -2
+            self.__harga = harga_baru
+
+    def info(self):
+        print(f"Laptop {self.merk} | RAM {self._ram}GB | Harga Rp{self.__harga}")
+
+
+class GamingLaptop(Laptop):
+    def __init__(self, merk, ram, harga, gpu):
+        super().__init__(merk, ram, harga)
+        self.gpu = gpu
+
+    def info(self):
+        print(f"Laptop Gaming {self.merk} | RAM {self._ram}GB | GPU {self.gpu} | Harga Rp{self.getHarga()}")
+
+
+l1 = Laptop("Lenovo", 8, 6500000)
+l2 = GamingLaptop("Asus ROG", 16, 20000000, "RTX 4060")
+
+l1.info()
+l2.info()
+
+print("\nHarga lama:", l1.getHarga())
+l1.setHarga(7000000)
+print("Harga baru:", l1.getHarga())
